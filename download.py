c18e47e05cacde85fe145b4cdd7c17d27be9f5d9
narlim <wangweimingooo@gmail.com>
2023-04-04 22:18:40 +0800

    fix file name

--- download.py
+++ download.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 import pyautogui
 import time
 
@@ -28,11 +29,12 @@ def perform_img(url):
     while True:
         image = wait.until(EC.element_to_be_clickable(("xpath", \
             '//div/div/div/div/img[not (@style="display: none;")]')))
+        vol = driver.find_element(By.XPATH, "//div[@class='reader--meta chapter']").text
         actions = ActionChains(driver)
         actions.context_click(image).perform()
 
         l = driver.current_url.split('/')
-        file_name = l[-2] + '-' + l[-1] + '.jpg'
+        file_name = vol + '-' + l[-1] + '.jpg'
         time.sleep(0.2)
         save(file_name)
         actions.move_to_element_with_offset(image, 300, 0).click().perform()
